dc2a9069535a4f61de2f76199490a07c3becea26
Vlad <salabunv@ukr.net>
2020-08-08 20:00:45 +0300

    додав коментарі

--- src/TelegramBotNotifier.php
+++ src/TelegramBotNotifier.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ *  Що корисного можна написати на початку файлу? Напевно
+ */
 namespace Salabun;
 
 /**
@@ -8,6 +10,10 @@ namespace Salabun;
 
 class TelegramBotNotifier
 {
+    
+    /**
+     *  Чи варто передавати токен у конструктор? Може краще в метод?
+     */
     public function __construct($token) 
     {
         $this->token = $token;
@@ -18,11 +24,17 @@ class TelegramBotNotifier
         $this->responses = [];
     }
     
+    /**
+     *  Додаю отримувача до списку:
+     */
     public function addRecipient($id) 
     {
         $this->recipients[] = $id;
     }
     
+    /**
+     *  Надсилаю повідомлення усім отримувачам:
+     */
     public function send() 
     {
         foreach($this->recipients as $recipient) {
@@ -46,9 +58,12 @@ class TelegramBotNotifier
             );
             
             curl_exec($ch);
+            
+            // Зберігаю відповідь сервера:
             $this->responses[$recipient][] = curl_getinfo($ch);
             curl_close($ch);
             
+            // Чекаю перед надсиланням наступного повідомлення:
             sleep($this->delay);
         
         }
@@ -56,60 +71,90 @@ class TelegramBotNotifier
         return $this->responses;
     }
 
+    /**
+     *  З нового рядка:
+     */
     public function br() 
     {
         $this->text .= PHP_EOL;
         return $this;
     }
-    
+   
+    /**
+     *  Додаю текст:
+     */
     public function text($text) 
     {
         $this->text .= $text;
         return $this;
     }
     
+    /**
+     *  Жирним:
+     */
     public function bold($text) 
     {
         $this->text .= '<b>' . $text . '</b>';
         return $this;
     }
     
+    /**
+     *  Жирним:
+     */
+    public function strong($text) 
+    {
+        $this->text .= '<strong>' . $text . '</strong>';
+        return $this;
+    }
+   
+    /**
+     *  Курсивом:
+     */
     public function italic($text) 
     {
         $this->text .= '<i>' . $text . '</i>';
         return $this;
     }
     
+    /**
+     *  Курсивом:
+     */
     public function em($text) 
     {
         $this->text .= '<em>' . $text . '</em>';
         return $this;
     }
     
-    public function strong($text) 
-    {
-        $this->text .= '<strong>' . $text . '</strong>';
-        return $this;
-    }
-    
+    /**
+     *  Виділяю код:
+     */
     public function code($text) 
     {
         $this->text .= '<code>' . $text . '</code>';
         return $this;
     }
     
+    /**
+     *  Форматую:
+     */
     public function pre($text) 
     {
         $this->text .= '<pre>' . $text . '</pre>';
         return $this;
     }
    
+    /**
+     *  Додаю посилання:
+     */
     public function url($array) 
     {
         $this->text .= '<a href="' . $array[0] . '">' . $array[1] . '</a>';
         return $this;
     }
     
+    /**
+     *  Відключаю попередній перегляд посилання:
+     */
     public function webPreview($boolean) 
     {
         if($boolean == true) {
